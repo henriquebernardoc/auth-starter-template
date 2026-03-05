b8ba58260ae7095948991cea8f55255c60269059
Henrique Bernardo <144061167+henriquebernardoc@users.noreply.github.com>
2026-03-05 11:39:44 -0300

    Rename Login.css to login.css

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { api } from "../../services/api";
-import './Login.css'
+import './login.css'
 
 function Login() {
     const navigate = useNavigate()
